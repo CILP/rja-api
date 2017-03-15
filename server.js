5513840bd27bfc415ee56cc4e34355a46599896f
propelics <propelics@customer-187-218-42-8.uninet-ide.com.mx>
2017-03-15 15:50:07 -0600

    add delete operation with and without params

--- server.js
+++ server.js
@@ -11,9 +11,19 @@ app.use(bodyParser.urlencoded({
   extended: true
 })); 
 
-Object.keys(configFile).forEach((key) => {
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
+// TODO: PUT, DELETE
+const setEndpoint = (key) => {
     const resource = configFile[key];
+
+    if (!resource.data && resource.dataUrl){
+        resource.data = JSON.parse(fs.readFileSync(resource.dataUrl, 'utf8'));
+    }
 
     if (resource.verb){
         resource.verb.forEach((httpMethod) => {
@@ -25,8 +35,28 @@ Object.keys(configFile).forEach((key) => {
                     resource.data.push(req.body);
                     res.json({status: 'success'});
                 });
-            } else {
+            } else if (lcHttp === 'get'){
                 app[lcHttp](`/${key.toLowerCase()}`, (req, res) => {
+                    res.json(resource.data);
+                });
+            } else if (lcHttp === 'delete'){
+
+                app[lcHttp](`/${key.toLowerCase()}/:id`, (req, res) => {
+
+                    const id = req.params.id ? +req.params.id : null;
+
+                    if (id){
+                        const deleted = resource.data.find(r => r.id === id);
+
+                        if (deleted){
+                            resource.data = resource.data.filter(r => r.id !== id);
+                            res.json(deleted);
+                        }
+                    }
+                });
+
+                app[lcHttp](`/${key.toLowerCase()}`, (req, res) => {
+                    resource.data = [];
                     res.json(resource.data);
                 });
             }
@@ -36,7 +66,9 @@ Object.keys(configFile).forEach((key) => {
             res.json(resource.data);
         });
     }
-});
+};
+
+Object.keys(configFile).forEach(setEndpoint);
 
 app.listen(rjaConfig.port, () => {
     console.info(`Server is runnig at port: ${rjaConfig.port}`);
